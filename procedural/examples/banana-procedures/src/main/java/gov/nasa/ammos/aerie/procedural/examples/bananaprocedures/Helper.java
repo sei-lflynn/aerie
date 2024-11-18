8f4b401992960229aa8e0e5d1ec07ae3da5325da
Theresa Kamerman <theresa.kamerman@jpl.nasa.gov>
2024-11-18 15:58:48 -0800

    Rename 'foo-procedures' to 'banana-procedures'
    These procedures use the Bananation mission model, not the Foo mission model

---
+++ procedural/examples/banana-procedures/src/main/java/gov/nasa/ammos/aerie/procedural/examples/bananaprocedures/Helper.java
@@ -0,0 +1,7 @@
+package gov.nasa.ammos.aerie.procedural.examples.bananaprocedures;
+
+public class Helper {
+  public static String greeting() {
+    return "hello from util";
+  }
+}
---
+++ procedural/examples/banana-procedures/src/main/java/gov/nasa/ammos/aerie/procedural/examples/bananaprocedures/constraints/Hello.java
@@ -0,0 +1,9 @@
+package gov.nasa.ammos.aerie.procedural.examples.bananaprocedures.constraints;
+
+import gov.nasa.ammos.aerie.procedural.examples.bananaprocedures.Helper;
+
+class Hello {
+  public static void main(String[] args) {
+    System.out.println(Helper.greeting());
+  }
+}
