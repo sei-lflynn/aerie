760c8a0dca3ccdfe84a186fc6b8cefcd1dbd6bee
Theresa Kamerman <theresa.kamerman@jpl.nasa.gov>
2024-11-18 16:15:31 -0800

    Remove Hello.java
    Was used as an early test of Jar building before there were goal classes, but is not needed anymore.

--- procedural/examples/banana-procedures/src/main/java/gov/nasa/ammos/aerie/procedural/examples/bananaprocedures/constraints/Hello.java
+++
@@ -1,9 +0,0 @@
-package gov.nasa.ammos.aerie.procedural.examples.bananaprocedures.constraints;
-
-import gov.nasa.ammos.aerie.procedural.examples.bananaprocedures.Helper;
-
-class Hello {
-  public static void main(String[] args) {
-    System.out.println(Helper.greeting());
-  }
-}
