92b14dca8ca9296af8dee5f0eec1e0c0f308df1b
rrgoetz <ryan.r.goetz@jpl.nasa.gov>
2025-02-12 07:20:32 -1000

    Added node testing system with a sample test

---
+++ action-server/tests/server.test.ts
@@ -0,0 +1,8 @@
+import test from 'node:test';
+import assert from 'assert';
+
+test('Unit Test', async () => {
+  await test('Parse the dictionary', async () => {
+        assert.equal("hello world", 'hello world');
+  }
+  )});
