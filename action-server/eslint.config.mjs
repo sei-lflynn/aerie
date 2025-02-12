ed8ec0b808b0726e00f0903e2332a6ea32647d27
rrgoetz <ryan.r.goetz@jpl.nasa.gov>
2025-02-12 07:19:01 -1000

    TS Action Server Node Project
    - Added prettier
    - Added eslint (eslint-sheriif)

---
+++ action-server/eslint.config.mjs
@@ -0,0 +1,14 @@
+import { sheriff, tseslint  } from 'eslint-config-sheriff';
+
+const sheriffOptions = {
+  "react": false,
+  "lodash": false,
+  "remeda": false,
+  "next": false,
+  "astro": false,
+  "playwright": false,
+  "jest": false,
+  "vitest": false
+};
+
+export default tseslint.config(sheriff(sheriffOptions));
