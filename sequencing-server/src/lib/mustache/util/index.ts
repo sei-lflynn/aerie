abaaf9c32e1410121ce56457a8aa92d1945c0aa9
Matthew Dailis <matthew.l.dailis@jpl.nasa.gov>
2025-03-07 13:50:52 -0800

    Add helpers

--- sequencing-server/src/lib/mustache/util/index.ts
+++ sequencing-server/src/lib/mustache/util/index.ts
@@ -1,10 +1,52 @@
 /* A wrapper for Handlebars, so that `sequencing-server` isn't polluted with helper registration and the like. */
 import Handlebars from "handlebars";
+import { addTime as addTimeFull, subtractTime as subtractTimeFull, InstanttoSTOL, STOLToInstant, SeqNToInstant, InstanttoSeqN, TextToISO8601 } from "./time.js";
 
-// somehow obtained
+
+// initialized to environment variable, but this can be changed at runtime.
 const environment = {
     language: "SEQN"
 }
+
+/////////////// AERIE HELPERS ///////////////
+// wrappers for helpers
+function addTime(startTime: string, duration: string) {
+    return addTimeFull(startTime, duration, environment)
+}
+
+function subtractTime(startTime: string, duration: string) {
+    return subtractTimeFull(startTime, duration, environment)
+}
+
+// helper to flatten out array
+function flatten(array: any[]): string {
+    if (environment.language === "STOL") {
+        return new Handlebars.SafeString(`[${array.join(", ")}]`).toString();
+    }
+    else {
+        return new Handlebars.SafeString(`[${array.join(" ")}]`).toString();
+    }
+}
+
+// helper to clean dates. must be manually invoked by the user, just in case. here, Text and SeqN are handled the same.
+function formatAsDate(date: string): string {
+    if (environment.language === "STOL") {
+        return InstanttoSTOL(STOLToInstant(date))
+    }
+    else if (environment.language === "Text") {
+        return TextToISO8601(date)
+    }
+    else {
+        return InstanttoSeqN(SeqNToInstant(date))
+    }
+}
+
+/////////////// AERIE HELPER REGISTRATION ///////////////
+Handlebars.registerHelper("add-time", addTime)
+Handlebars.registerHelper("subtract-time", subtractTime)
+Handlebars.registerHelper("flatten", flatten)
+Handlebars.registerHelper("format-as-date", formatAsDate)
+
 
 /////////////// EXPOSE TO SEQUENCING-SERVER ///////////////
 export class Mustache {
@@ -16,7 +58,6 @@ export class Mustache {
     }
 
     public execute(data: any) {
-        // TODO: AUTOMATICALLY FORMAT TIMES IN DATA? Ask about this
         return this.template(data)
     }
 
