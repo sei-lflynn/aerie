940fc187f738f80a8ba983033b36da2536418dfc
Theresa Kamerman <theresa.kamerman@jpl.nasa.gov>
2025-03-11 14:23:22 -0700

    Update ConstraintRecord
    Code Pattern now matches the one used for Procedural Constraints, with the definition stored within a Type interface
    
    Similarly, Constraint and ConstraintRecord have been merged, as they contain the exact same fields.

--- merlin-server/src/main/java/gov/nasa/jpl/aerie/merlin/server/remotes/postgres/ConstraintRecord.java
+++
@@ -1,8 +0,0 @@
-package gov.nasa.jpl.aerie.merlin.server.remotes.postgres;
-
-public record ConstraintRecord(
-    long id,
-    long revision,
-    String name,
-    String description,
-    String definition) {}
