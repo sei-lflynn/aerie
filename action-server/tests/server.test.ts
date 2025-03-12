de0f0806b72761d109331031151fc92923216f60
rrgoetz <ryan.r.goetz@jpl.nasa.gov>
2025-03-12 10:47:19 -1000

    rename the unit test.

--- action-server/tests/server.test.ts
+++ action-server/tests/server.test.ts
@@ -1,8 +1,8 @@
-import test from 'node:test';
-import assert from 'assert';
+import test from "node:test";
+import assert from "assert";
 
-test('Unit Test', async () => {
-  await test('Parse the dictionary', async () => {
-        assert.equal("hello world", 'hello world');
-  }
-  )});
+test("Unit Test", async () => {
+  await test("example test", async () => {
+    assert.equal("hello world", "hello world");
+  });
+});
