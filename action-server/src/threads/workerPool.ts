1997033612b20a2f1419a861164717673af8a356
rrgoetz <ryan.r.goetz@jpl.nasa.gov>
2025-03-12 09:20:08 -1000

    Pull docker configuration for worker thread count

--- action-server/src/threads/workerPool.ts
+++ action-server/src/threads/workerPool.ts
@@ -1,31 +1,30 @@
-
 import { Piscina } from "piscina";
 import * as path from "node:path";
-import { ActionTask } from "../type/types";
+import { ActionResponse, ActionTask } from "../type/types";
+import { configuration } from "../config";
 
 export class ActionWorkerPool {
-  private static piscina : Piscina<any, any>;
+  private static piscina: Piscina<any, any>;
 
-  static setup(){
+  static setup() {
     ActionWorkerPool.piscina = new Piscina({
       filename: path.resolve(__dirname, "worker.js"),
-      maxThreads: 1,
-      minThreads: 1
+      maxThreads: parseInt(configuration().ACTION_MAX_WORKER_NUM),
+      minThreads: parseInt(configuration().ACTION_WORKER_NUM),
     });
   }
 
-  static async submitTask(task:ActionTask){
+  static async submitTask(task: ActionTask): Promise<ActionResponse> {
+    // todo: maintain a custom queue for enqueueing run requests *by workspace*
+    if (!ActionWorkerPool.piscina) {
+      throw new Error("Worker pool not initialized");
+    }
+
     try {
-      const result = await ActionWorkerPool.piscina.run(task,
-          { name: 'runAction' }
-      );
-      return result;
+      return await ActionWorkerPool.piscina.run(task, { name: "runAction" });
     } catch (error) {
-      console.error('Task submission failed:', error);
+      console.error("Task submission failed:", error);
       throw error;
     }
   }
 }
-
-
-
