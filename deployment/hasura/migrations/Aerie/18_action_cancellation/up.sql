a9932a6b9dcbad74cf0d1e8833e08cd40ff14fa9
jhaug <jhaug@jpl.nasa.gov>
2025-04-23 16:02:08 -0700

    Added migration 18 for addition of the canceled key and trigger function to action_run

---
+++ deployment/hasura/migrations/Aerie/18_action_cancellation/up.sql
@@ -0,0 +1,33 @@
+alter table actions.action_run
+add column canceled boolean not null default false,
+;
+
+comment on column actions.action_run.canceled is e''
+  'Whether the user has requested that this action be cancelled.';
+
+create function actions.notify_action_run_cancel_requested()
+  returns trigger
+  security definer
+  language plpgsql as $$
+begin
+  -- Notify if canceled is set to true
+  if NEW.canceled then
+    perform pg_notify('action_run_cancel_requested', json_build_object(
+        'action_run_id', NEW.id,
+        'canceled', NEW.canceled
+    )::text);
+  end if;
+
+  return null;
+end$$;
+
+create trigger notify_action_run_cancel_requested
+  after update on actions.action_run
+  for each row
+  when (
+    NEW.canceled = true
+      and OLD.canceled is distinct from NEW.canceled
+    )
+execute function actions.notify_action_run_cancel_requested();
+
+call migrations.mark_migration_applied('18');
