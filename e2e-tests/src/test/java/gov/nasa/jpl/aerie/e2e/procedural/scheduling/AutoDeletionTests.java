8bd05224a01ef84039424ea2ac35912825fe83d9
joelco <joel.courtney@jpl.nasa.gov>
2025-05-13 16:46:47 -0700

    Fix PR comments

--- e2e-tests/src/test/java/gov/nasa/jpl/aerie/e2e/procedural/scheduling/AutoDeletionTests.java
+++ e2e-tests/src/test/java/gov/nasa/jpl/aerie/e2e/procedural/scheduling/AutoDeletionTests.java
@@ -9,9 +9,11 @@ import org.junit.jupiter.api.Test;
 import javax.json.Json;
 import java.io.IOException;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AutoDeletionTests extends ProceduralSchedulingSetup {
@@ -30,6 +32,16 @@ public class AutoDeletionTests extends ProceduralSchedulingSetup {
               startsAt:TimingConstraint.singleton(WindowProperty.START).plus(Temporal.Duration.from({ minutes : 5}))
             })
           }""";
+
+      /*
+      Schedule logic:
+      1. Use an eDSL coexistence goal that creates a GrowBanana for every BiteBanana
+         This will do nothing on the first run, and may or may not do anything depending on
+         the 2nd goal's auto-deletion behavior being tested.
+      2. Use a procedural goal that always places a single BiteBanana, with auto-deletion.
+         Takes a `deleteAtBeginning` argument to set whether the auto-deletion happens at
+         the beginning of the run (before goal 1) or just before (in between 1 and 2).
+       */
 
       edslId = hasura.createSchedulingSpecGoal(
           "Coexistence Scheduling Test Goal",
@@ -58,6 +70,8 @@ public class AutoDeletionTests extends ProceduralSchedulingSetup {
     hasura.deleteSchedulingGoal(edslId.goalId());
   }
 
+  // Just checking that the basic goal works as expected on a single run.
+  // This doesn't check any interesting auto-deletion stuff.
   @Test
   void createsOneActivityIfRunOnce() throws IOException {
     final var args = Json
@@ -79,6 +93,10 @@ public class AutoDeletionTests extends ProceduralSchedulingSetup {
     ));
   }
 
+  // Test that if the auto-deletion is set to "just before", goal 1 sees the
+  // activity created by goal 2 in the previous run, and creates a corresponding activity.
+  // Scheduling is run several extra times to make sure it has achieved steady-state
+  // after the first two runs.
   @Test
   void createsTwoActivitiesSteadyState_JustBefore() throws IOException {
     final var args = Json
@@ -90,6 +108,10 @@ public class AutoDeletionTests extends ProceduralSchedulingSetup {
 
     hasura.awaitScheduling(specId);
 
+    var oldBiteId = hasura.getPlan(planId).activityDirectives().getFirst().id();
+
+    var oldGrowId = -1;
+
     for (int i = 0; i < 3; i++) {
       hasura.updatePlanRevisionSchedulingSpec(planId);
       hasura.awaitScheduling(specId);
@@ -97,18 +119,49 @@ public class AutoDeletionTests extends ProceduralSchedulingSetup {
       final var plan = hasura.getPlan(planId);
       final var activities = plan.activityDirectives();
 
+      final AtomicInteger newBiteId = new AtomicInteger();
+      final AtomicInteger newGrowId = new AtomicInteger();
+
       assertEquals(2, activities.size());
 
       assertTrue(activities.stream().anyMatch(
-          it -> Objects.equals(it.type(), "BiteBanana")
+          it -> {
+            final var isBite = Objects.equals(it.type(), "BiteBanana");
+            if (isBite) {
+              newBiteId.set(it.id());
+            }
+            return isBite;
+          }
       ));
 
       assertTrue(activities.stream().anyMatch(
-          it -> Objects.equals(it.type(), "GrowBanana")
+          it -> {
+            final var isGrow = Objects.equals(it.type(), "GrowBanana");
+            if (isGrow) {
+              newGrowId.set(it.id());
+            }
+            return isGrow;
+          }
       ));
+
+      // Makes sure the bite banana is a new activity every time.
+      assertNotEquals(oldBiteId, newBiteId.get());
+
+      if (oldGrowId != -1) {
+        assertEquals(oldGrowId, newGrowId.get());
+      } else {
+        oldGrowId = newGrowId.get();
+      }
+
+      oldBiteId = newBiteId.get();
     }
   }
 
+  // Test that if the auto-deletion is set to "at beginning", goal 1 does not
+  // see the activity created by goal 2 in the previous run and does not create
+  // an activity. Goal 2 should still auto-delete and replace with a single activity.
+  // Scheduling is run several extra times to make sure it has achieved steady state
+  // after the first run.
   @Test
   void createsOneActivitySteadyState_AtBeginning() throws IOException {
     final var args = Json
