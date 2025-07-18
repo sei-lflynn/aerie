3859618e43bdd6f55b89e54ef601dbb649e1dbec
Timofej Ratsko <Timofej.Ratsko@jpl.nasa.gov>
2025-07-18 11:37:15 -0700

    Add subsystem to e2e tests

--- e2e-tests/src/test/java/gov/nasa/jpl/aerie/e2e/types/ActivityType.java
+++ e2e-tests/src/test/java/gov/nasa/jpl/aerie/e2e/types/ActivityType.java
@@ -5,12 +5,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-public record ActivityType(String name, Map<String, Parameter> parameters, ValueSchema computedAttributes) {
+public record ActivityType(String name, Map<String, Parameter> parameters, ValueSchema computedAttributes, String subsystem) {
   /**
    * Create an ActivityType with an empty computed attributes value schema.
    */
+  public ActivityType(final String name, final Map<String, Parameter> parameters, final String subsystem) {
+    this(name, parameters, new ValueSchema.ValueSchemaStruct(Map.of()), subsystem);
+  }
+
+  /**
+   * Create an ActivityType with an empty computed attributes value schema and no subsystem.
+   */
   public ActivityType(final String name, final Map<String, Parameter> parameters) {
-    this(name, parameters, new ValueSchema.ValueSchemaStruct(Map.of()));
+    this(name, parameters, new ValueSchema.ValueSchemaStruct(Map.of()), null);
   }
 
   public static ActivityType fromJSON(JsonObject json) {
@@ -19,7 +26,11 @@ public record ActivityType(String name, Map<String, Parameter> parameters, Value
     for (final var parameterName : parameters.keySet()) {
       parameterMap.put(parameterName, Parameter.fromJSON(parameters.getJsonObject(parameterName)));
     }
-    return new ActivityType(json.getString("name"), parameterMap, ValueSchema.fromJSON(json.getJsonObject("computed_attributes_value_schema")));
+    final var subsystem = json.isNull("subsystem") ? null : json.getJsonObject("subsystem").getString("name");
+    return new ActivityType(json.getString("name"),
+                            parameterMap,
+                            ValueSchema.fromJSON(json.getJsonObject("computed_attributes_value_schema")),
+                            subsystem);
   }
 
   public record Parameter(int order, ValueSchema schema) {
