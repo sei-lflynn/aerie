8ee6b40b4765fa06209ac37c8dd958979fdbc957
Theresa Kamerman <theresa.kamerman@jpl.nasa.gov>
2025-08-01 14:34:46 -0700

    Add class-level doc comments to DirectoryTree
    - Validate that `root` is actually always a directory

--- workspace-server/src/main/java/gov/nasa/jpl/aerie/workspace/server/DirectoryTree.java
+++ workspace-server/src/main/java/gov/nasa/jpl/aerie/workspace/server/DirectoryTree.java
@@ -11,12 +11,28 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * A tree storing
+ * A tree representing the contents of a directory on the file system.
+ *
+ * Used to generate a JSON object for the listFiles endpoint.
+ *
+ * Does not use a preexisting implementation like Apache's FileUtils, as the focus of this class is to convert
+ * the results of `Files.walk` into a JSON Object.
  */
 public class DirectoryTree {
   private final DirectoryNode root;
 
+  /**
+   * Generate a DirectoryTree.
+   *
+   * @param root the root directory of the DirectoryTree.
+   * @param inputList a list of Paths contained within the root directory
+   * @param extensionMappings a map of file extensions to RenderTypes.
+   *    Used to determine the RenderType of file paths
+   */
   public DirectoryTree(Path root, List<Path> inputList, Map<String, RenderType> extensionMappings) {
+    if(!root.toFile().isDirectory()) {
+      throw new IllegalArgumentException("Cannot create a DirectoryTree from a file.");
+    }
     this.root = new DirectoryNode(root);
 
     for(final var path : inputList){
